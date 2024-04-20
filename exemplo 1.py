f012ff39c3909d36dc7c8a09fbc3af6c339addff
Kelly Brito <kellybritoqa2023@gmail.com>
2024-04-20 11:01:31 +0100

    aula calculos

---
+++ exemplo 1.py
@@ -0,0 +1,13 @@
+#Faca um programa de conta
+
+n1=float(input('digite o 1º numero: '))
+n2=float(input('digite o 2º numero: '))
+soma=n1+n2
+sub=n1-n2
+mult=n1*n2
+div=n1/n2
+print('\n' ,n1)
+print('\n' ,n1, '+',n2,'=',soma)
+print('\n' ,n1, '-',n2,'=',sub)
+print('\n' ,n1, '*',n2,'=',mult)
+print('\n' ,n1, '/',n2,'=',div)
